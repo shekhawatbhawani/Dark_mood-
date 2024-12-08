9032520d15ad60205e09304be0a416f4ea305d95
Shekhawatbhawanisingh <shekhawatbhwanisingh49@gmail.com>
2024-12-08 20:55:01 +0530

    Dark mood button

---
+++ script.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector('.bap')
+let body = document.querySelector("body")
+let raj = document.querySelector("#raj")
+let kk = document.querySelector(".kk")
+btn.addEventListener("click",()=>{
+   
+    body.classList.toggle("body")
+
+    raj.classList.toggle("bap")
+
+    kk.classList.toggle("ch")
+
+})
